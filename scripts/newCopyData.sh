d40f36c8f72798b5ab351fa1970fa76ebd5f267e
Leo <328264155@qq.com>
2023-12-06 05:49:24 -0500

    kaldi_utils

---
+++ scripts/newCopyData.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Copyright xmuspeech (Author:Snowdar 2018-7-25)
+
+topdir=data
+clean=true
+force=false # for overwrite
+
+. scripts/parse_options.sh || exit 1
+
+if [[ $# != 2 ]];then
+echo "[exit] Num of parameters is not equal to 2"
+echo "usage:$0 --force [false|true] <prefixes> <src-dirs>"
+exit 1
+fi
+
+pre=$1
+srcs=$2
+
+for src in $srcs;do
+[ ! -d "$src" ] && tmp=$src && src=$topdir/$src && [ ! -d "$src" ] && echo "[exit] No such dir $tmp or $src" && exit 1
+name=`basename $src`
+target=$topdir/${pre}/$name
+
+[ "$src" == "$target" ] && echo "[Warning] data-dir $src is same to target-data-dir, so skip it" && continue
+[ -d $target ] && [ "$force" == "false" ] && echo "[exit] $target is exist, please delete it carefully by yourself" && exit 1
+
+rm -rf $target
+mkdir -p $target
+
+echo "Copy $src to $target..."
+
+for x in wav.scp utt2spk spk2utt;do
+[ ! -f $src/$x ] && echo "[exit] Expected $src/$x to exist at least." && exit 1
+done
+
+trials=""
+for path in $(find $src -name "*trials");do
+trials="$trials $(basename $path)"
+done
+
+extra=
+[ "$clean" == "false" ] && extra="feats.scp vad.scp"
+for x in wav.scp utt2spk spk2utt utt2num_frames utt2dur reco2dur text utt2gender spk2gender $extra $trials;do
+[ -f $src/$x ] && cp $src/$x $target/ && echo "[ $x ] copy done"
+done
+echo "Copy done. Your new datadir is $target."
+done
