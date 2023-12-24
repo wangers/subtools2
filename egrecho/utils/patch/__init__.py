27c8ff870ffee02075c8946c94e85de5002ed250
Leo <328264155@qq.com>
2023-12-24 04:11:38 -0500

    remove dumplicate

--- egrecho/utils/patch/__init__.py
+++ egrecho/utils/patch/__init__.py
@@ -1,4 +1,3 @@
-from egrecho.utils.patch.google_doc import compose, parse
 from egrecho.utils.patch.io_patch import FsspecLocalGlob, gzip_open_patch
 from egrecho.utils.patch.simple_parse_patch.decoding import register_decoding_fn
 from egrecho.utils.patch.simple_parse_patch.serializable import (
@@ -15,13 +14,11 @@ from egrecho.utils.patch.torchdata_patch import (
 
 __all__ = [
     "asdict_filt",
-    "compose",
     "default_value",
     "from_dict",
     "FsspecLocalGlob",
     "gzip_open_patch",
     "is_stream_handle",
-    "parse",
     "register_decoding_fn",
     "StreamWrapper",
     "validate_input_col",
